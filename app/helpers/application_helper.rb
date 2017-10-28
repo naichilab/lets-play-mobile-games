99e41fc3286ab7d11505fd3235992db9057d330b
naichilab <naichilab@live.jp>
2017-10-29 03:32:35 +0900

    説明文

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返す
   def full_title(html_title = '')
-    base_title = "そのゲーム、実況できる？ - 実況OKスマホゲーム一覧"
+    base_title = "そのゲーム、実況できる？ - 実況OKなスマホゲームを集めました"
     if html_title.empty?
       base_title
     else
