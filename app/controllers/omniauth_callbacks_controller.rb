cde0963a338740a8044572f2665f7c6c2b72a1c1
naichilab <naichilab@live.jp>
2017-10-29 06:54:55 +0900

    メッセージテキスト修正

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,6 +4,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # @user = User.from_omniauth(request.env["omniauth.auth"].except("extra"))
 
     if @user.persisted?
+      flash[:notice] = "ログインしました。"
       sign_in_and_redirect @user
     else
       session["devise.user_attributes"] = @user.attributes
