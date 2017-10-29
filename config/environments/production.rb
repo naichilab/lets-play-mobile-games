286f8036579e048a21d0e01cd9adffa809f3ef63
naichilab <naichilab@live.jp>
2017-10-29 09:23:04 +0900

    set hostname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rails-vue-sandbox_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {host: ENV['HOST_NAME']}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address => 'smtp.sendgrid.net',
