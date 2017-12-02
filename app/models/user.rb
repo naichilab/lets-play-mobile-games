15c344ec847096ffd7e16feb03a80d70ec92a892
naichilab <naichilab@live.jp>
2017-12-02 14:29:56 +0900

    admin user model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ApplicationRecord
+  TEMP_EMAIL_PREFIX = 'change-me@'
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :omniauthable, omniauth_providers: [:twitter]
 
-  TEMP_EMAIL_PREFIX = 'change-me@'
 
+  # validates
+  validates :username, presence: true, length: {maximum: 32}
+
+  # relations
   has_many :games, dependent: :destroy
+
 
   def self.new_token
     SecureRandom.urlsafe_base64
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe User do
+  describe "ファクトリ" do
+    it "通常ユーザー" do
+      user = build(:user)
+      expect(user).to be_valid
+      expect(user.admin).to be_falsey
+    end
+    it "管理者ユーザー" do
+      admin_user = build(:admin_user)
+      expect(admin_user).to be_valid
+      expect(admin_user.admin).to be_truthy
+    end
+    it "無効なユーザー" do
+      invalid_user = build(:invalid_user)
+      expect(invalid_user).not_to be_valid
+    end
+  end
+
+  describe "バリデーション" do
+    describe ":username" do
+      let(:user) {build(:user, username: username)}
+      subject {user}
+
+      context "空白の場合" do
+        let(:username) {"    "}
+        it {is_expected.not_to be_valid}
+      end
+
+      context "33文字以上の場合" do
+        let(:username) {"a" * 33}
+        it {is_expected.not_to be_valid}
+      end
+    end
+  end
+
+end
