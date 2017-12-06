7b5d1cb0e9d4005a4647d6cd70bbd31a60176b4f
naichilab <naichilab@live.jp>
2017-12-07 01:19:50 +0900

    色々除外

--- app/javascript/packs/game_search.vue
+++ app/javascript/packs/game_search.vue
@@ -3,15 +3,18 @@
         <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
             <h2>実況可否</h2>
             <div>
-                <input type="radio" id="radPermissionAll" value="" v-model="formValues.permission" v-on:change="getGames">
+                <input type="radio" id="radPermissionAll" value="" v-model="formValues.permission"
+                       v-on:change="getGames">
                 <label for="radPermissionAll">すべて</label>
             </div>
             <div>
-                <input type="radio" id="radPermissionOK" value="true" v-model="formValues.permission" v-on:change="getGames">
+                <input type="radio" id="radPermissionOK" value="true" v-model="formValues.permission"
+                       v-on:change="getGames">
                 <label for="radPermissionOK">実況OK</label>
             </div>
             <div>
-                <input type="radio" id="radPermissionNG" value="false" v-model="formValues.permission" v-on:change="getGames">
+                <input type="radio" id="radPermissionNG" value="false" v-model="formValues.permission"
+                       v-on:change="getGames">
                 <label for="radPermissionNG">実況NG</label>
             </div>
             <h2>プラットフォーム</h2>
@@ -20,11 +23,13 @@
                 <label for="radPlatformAll">すべて</label>
             </div>
             <div>
-                <input type="radio" id="radPlatformAndroid" value="android" v-model="formValues.platform" v-on:change="getGames">
+                <input type="radio" id="radPlatformAndroid" value="android" v-model="formValues.platform"
+                       v-on:change="getGames">
                 <label for="radPlatformAndroid">Android</label>
             </div>
             <div>
-                <input type="radio" id="radPlatformIOS" value="ios" v-model="formValues.platform" v-on:change="getGames">
+                <input type="radio" id="radPlatformIOS" value="ios" v-model="formValues.platform"
+                       v-on:change="getGames">
                 <label for="radPlatformIOS">iOS</label>
             </div>
             <h2>ジャンル</h2>
@@ -33,7 +38,8 @@
                 <label for="radCategoryAll">すべて</label>
             </div>
             <div v-for="category in masterData.categories">
-                <input type="radio" :id="'radCategory' + category.id" :value="category.id" v-model="formValues.category" v-on:change="getGames">
+                <input type="radio" :id="'radCategory' + category.id" :value="category.id" v-model="formValues.category"
+                       v-on:change="getGames">
                 <label :for="'radCategory' + category.id" v-text="category.name"></label>
             </div>
         </div>
@@ -46,32 +52,33 @@
                         <img class="game-icon-image" :src="game.icon.thumb.url">
                     </div>
                     <div class="row">
-                        <div class="col-xs-10  col-xs-offset-1">
+                        <div class="col-xs-10 col-xs-offset-1 ">
 
                             <!-- Start of the content -->
                             <div class="post-content--front-page">
                                 <h1 class="front-page-title">
-                                    <a href="/games/3" v-text="game.title"></a>
+                                    <a :href="'/games/' + game.id" v-text="game.title"></a>
                                 </h1>
-                                <p>
-                                    <span v-if="game.permission" class="label label-primary">実況OK</span>
-                                    <span v-else="" class="label label-danger">実況NG</span>
-                                </p>
-                                <p>
-                                    <span v-if="game.android_url != ''" class="label label-default">Android</span>
-                                    <span v-if="game.ios_url != ''" class="label label-default">iOS</span>
-                                </p>
+                                <!--<p>-->
+                                <!--<span v-if="game.permission" class="label label-primary">実況OK</span>-->
+                                <!--<span v-else="" class="label label-danger">実況NG</span>-->
+                                <!--</p>-->
+                                <!--<p>-->
+                                <!--<span v-if="game.android_url != ''" class="label label-default">Android</span>-->
+                                <!--<span v-if="game.ios_url != ''" class="label label-default">iOS</span>-->
+                                <!--</p>-->
                             </div>
 
                             <!-- End of the content -->
-                            <a :href="'/games/' + game.id">
-                                <div class="read-more">
-                                    詳細
-                                    <div class="read-more__arrow">
-                                        <i class="fa fa-chevron-right"></i>
-                                    </div>
-                                </div>
-                            </a></div>
+                            <!--<a :href="'/games/' + game.id">-->
+                                <!--<div class="read-more">-->
+                                    <!--詳細-->
+                                    <!--<div class="read-more__arrow">-->
+                                        <!--<i class="fa fa-chevron-right"></i>-->
+                                    <!--</div>-->
+                                <!--</div>-->
+                            <!--</a>-->
+                        </div>
                     </div>
                 </div>
             </div>
@@ -120,5 +127,8 @@
 <style scoped>
     .front-page-title {
         font-size: 1em;
+    }
+    .post-content--front-page{
+        margin-bottom :0;
     }
 </style>
