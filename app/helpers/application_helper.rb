b3549fbd479eca2e277c33ef211bb76a98330808
naichilab <naichilab@live.jp>
2017-12-13 01:28:55 +0900

    fix #46 サイトタイトル変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返す
   def full_title(html_title = '')
-    base_title = "そのゲーム、実況できる？ - 実況OKなスマホゲームを集めました"
+    base_title = "ゲームを実況しよう！ - 実況OKなゲームを集めました"
     if html_title.empty?
       base_title
     else
--- spec/features/public_pages_spec.rb
+++ spec/features/public_pages_spec.rb
@@ -4,9 +4,7 @@ feature "Public pages" do
 
   scenario "ホームを表示" do
     visit root_path
-    expect(page).to have_title "そのゲーム、実況できる？ - 実況OKなスマホゲームを集めました"
-    # click_link 'ゲーム一覧'
-    # expect(page).to have_title "登録ゲーム一覧 | そのゲーム、実況できる？ - 実況OKなスマホゲームを集めました"
+    expect(page).to have_title "ゲームを実況しよう！ - 実況OKなゲームを集めました"
   end
 
 end
