1519be777193a9ba385af620f0e9c3e42625d5ae
naichilab <naichilab@live.jp>
2017-12-15 01:32:20 +0900

    #62 model, factory, model spec

--- spec/factories/platforms.rb
+++ spec/factories/platforms.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :platform do
-    code "MyString"
-    name "MyString"
+    sequence(:code) {|n| "platform#{n}"}
+    sequence(:name) {|n| "プラットフォーム#{n}"}
+
+    factory :invalid_platform do
+      code "  "
+    end
+    
   end
 end
--- spec/models/platform_spec.rb
+++ spec/models/platform_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
-RSpec.describe Platform, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Platform do
+  describe "ファクトリ" do
+    context "正常なデータ" do
+      let(:platform) {build(:platform)}
+      subject {platform}
+      it {is_expected.to be_valid}
+    end
+    context "異常なデータ" do
+      let(:invalid_platform) {build(:invalid_platform)}
+      subject {invalid_platform}
+      it {is_expected.not_to be_valid}
+    end
+  end
+  describe "バリデーション" do
+    describe ":code" do
+      let(:platform) {build(:platform, code: code)}
+      subject {platform}
+
+      context "空白の場合" do
+        let(:code) {"  "}
+        it {is_expected.not_to be_valid}
+      end
+      context "17文字以上の場合" do
+        let(:code) {"a" * 17}
+        it {is_expected.not_to be_valid}
+      end
+    end
+    describe ":name" do
+      let(:platform) {build(:platform, name: name)}
+      subject {platform}
+
+      context "空白の場合" do
+        let(:name) {"  "}
+        it {is_expected.not_to be_valid}
+      end
+      context "33文字以上の場合" do
+        let(:name) {"a" * 33}
+        it {is_expected.not_to be_valid}
+      end
+    end
+  end
 end
