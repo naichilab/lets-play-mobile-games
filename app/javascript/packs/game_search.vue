184f46ca78abfdb4d5d2cc50c173dfc0fb5e71da
Subarunari <koyadouhu@gmail.com>
2017-12-15 17:12:56 +0900

    #41 add responsive style for search area

--- app/javascript/packs/game_search.vue
+++ app/javascript/packs/game_search.vue
@@ -1,81 +1,89 @@
 <template>
     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
-            <div class="panel panel-default condition-box box-joint">
-                <div class="panel-heading search-condition-title" data-toggle="collapse" data-target="#guideline-condition" aria-expanded="true" aria-controls="guideline-chevron">
-                    <i class="fa fa-caret-right" aria-hidden="true"></i>
-                    <i class="fa fa-caret-down" aria-hidden="true"></i>
-                    ガイドライン
-                </div>
-                <ul class="list-group collapse in" id="guideline-condition">
-                    <li class="condition-item">
-                        <input type="radio" id="radPermissionAll" value="" v-model="formValues.permission"
-                                                                           v-on:change="getGames">
-                        <label for="radPermissionAll">すべて</label>
-                    </li>
-                    <li class="condition-item">
-                        <input type="radio" id="radPermissionOK" value="true" v-model="formValues.permission"
-                                                                              v-on:change="getGames">
-                        <label for="radPermissionOK">
-                            <i class="fa fa-warning fa-lg text-warning" aria-hidden="true"></i>あり
-                        </label>
-                    </li>
-                    <li class="condition-item">
-                        <input type="radio" id="radPermissionNG" value="false" v-model="formValues.permission"
+            <div class="col-xs-12 col-sm-12 col-md-4 col-lg-3">
+            <input type="checkbox" id="filtering" class="hidden-element condition-filter-area">
+            <label for="filtering" class="btn btn-warning btn-block condition-filter-area filter-btn">
+                <i class="fa fa-filter"></i>条件で絞りこみ
+            </label>
+
+            <div class="condition-area">
+                <div class="panel panel-default condition-box box-joint">
+                    <div class="panel-heading search-condition-title" data-toggle="collapse" data-target="#guideline-condition" aria-expanded="true" aria-controls="guideline-chevron">
+                        <i class="fa fa-caret-right" aria-hidden="true"></i>
+                        <i class="fa fa-caret-down" aria-hidden="true"></i>
+                        ガイドライン
+                    </div>
+                    <ul class="list-group collapse in" id="guideline-condition">
+                        <li class="condition-item">
+                            <input type="radio" id="radPermissionAll" value="" v-model="formValues.permission"
                                                                                v-on:change="getGames">
-                        <label for="radPermissionNG">
-                            <i class="fa fa-check-circle-o fa-lg text-success" aria-hidden="true"></i>なし
-                        </label>
-                    </li>
-                </ul>
-            </div>
-            <div class="panel panel-default condition-box box-joint">
-                <div class="panel-heading search-condition-title" data-toggle="collapse" data-target="#platform-condition" aria-expanded="true" aria-controls="platform-chevron">
-                    <i class="fa fa-caret-right" aria-hidden="true"></i>
-                    <i class="fa fa-caret-down" aria-hidden="true"></i>
-                    プラットフォーム
+                            <label for="radPermissionAll">すべて</label>
+                        </li>
+                        <li class="condition-item">
+                            <input type="radio" id="radPermissionOK" value="true" v-model="formValues.permission"
+                                                                                  v-on:change="getGames">
+                            <label for="radPermissionOK">
+                                <i class="fa fa-warning fa-lg text-warning" aria-hidden="true"></i>あり
+                            </label>
+                        </li>
+                        <li class="condition-item">
+                            <input type="radio" id="radPermissionNG" value="false" v-model="formValues.permission"
+                                                                                   v-on:change="getGames">
+                            <label for="radPermissionNG">
+                                <i class="fa fa-check-circle-o fa-lg text-success" aria-hidden="true"></i>なし
+                            </label>
+                        </li>
+                    </ul>
                 </div>
-                <ul class="list-group collapse in" id="platform-condition">
-                    <li class="condition-item">
-                        <input type="radio" id="radPlatformAll" value="" v-model="formValues.platform" v-on:change="getGames">
-                        <label for="radPlatformAll">すべて</label>
-                    </li>
-                    <li class="condition-item">
-                        <input type="radio" id="radPlatformAndroid" value="android" v-model="formValues.platform"
-                                                                                    v-on:change="getGames">
-                        <label for="radPlatformAndroid">
-                            <i class="fa fa-android fa-lg android-statue-logo" aria-hidden="true"></i>Android
-                        </label>
-                    </li>
-                    <li class="condition-item">
-                        <input type="radio" id="radPlatformIOS" value="ios" v-model="formValues.platform"
-                                                                            v-on:change="getGames">
-                        <label for="radPlatformIOS">
-                            <i class="fa fa-apple fa-lg apple-logo" aria-hidden="true"></i>iOS
-                        </label>
-                    </li>
-                </ul>
+                <div class="panel panel-default condition-box box-joint">
+                    <div class="panel-heading search-condition-title" data-toggle="collapse" data-target="#platform-condition" aria-expanded="true" aria-controls="platform-chevron">
+                        <i class="fa fa-caret-right" aria-hidden="true"></i>
+                        <i class="fa fa-caret-down" aria-hidden="true"></i>
+                        プラットフォーム
+                    </div>
+                    <ul class="list-group collapse in" id="platform-condition">
+                        <li class="condition-item">
+                            <input type="radio" id="radPlatformAll" value="" v-model="formValues.platform" v-on:change="getGames">
+                            <label for="radPlatformAll">すべて</label>
+                        </li>
+                        <li class="condition-item">
+                            <input type="radio" id="radPlatformAndroid" value="android" v-model="formValues.platform"
+                                                                                        v-on:change="getGames">
+                            <label for="radPlatformAndroid">
+                                <i class="fa fa-android fa-lg android-statue-logo" aria-hidden="true"></i>Android
+                            </label>
+                        </li>
+                        <li class="condition-item">
+                            <input type="radio" id="radPlatformIOS" value="ios" v-model="formValues.platform"
+                                                                                v-on:change="getGames">
+                            <label for="radPlatformIOS">
+                                <i class="fa fa-apple fa-lg apple-logo" aria-hidden="true"></i>iOS
+                            </label>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="panel panel-default condition-box">
+                    <div class="panel-heading search-condition-title" data-toggle="collapse" data-target="#genre-condition" aria-expanded="true" aria-controls="genre-chevron">
+                        <i class="fa fa-caret-right" aria-hidden="true"></i>
+                        <i class="fa fa-caret-down" aria-hidden="true"></i>
+                        ジャンル
+                    </div>
+                    <ul class="list-group collapse in" id="genre-condition">
+                        <li class="condition-item">
+                            <input type="radio" id="radCategoryAll" value="" v-model="formValues.category" v-on:change="getGames">
+                            <label for="radCategoryAll">すべて</label>
+                        </li>
+                        <li class="condition-item" v-for="category in masterData.categories">
+                            <input type="radio" :id="'radCategory' + category.id" :value="category.id" v-model="formValues.category"
+                                                                                                       v-on:change="getGames">
+                            <label :for="'radCategory' + category.id" v-text="category.name"></label>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             </div>
 
-            <div class="panel panel-default condition-box">
-                <div class="panel-heading search-condition-title" data-toggle="collapse" data-target="#genre-condition" aria-expanded="true" aria-controls="genre-chevron">
-                    <i class="fa fa-caret-right" aria-hidden="true"></i>
-                    <i class="fa fa-caret-down" aria-hidden="true"></i>
-                    ジャンル
-                </div>
-                <ul class="list-group collapse in" id="genre-condition">
-                    <li class="condition-item">
-                        <input type="radio" id="radCategoryAll" value="" v-model="formValues.category" v-on:change="getGames">
-                        <label for="radCategoryAll">すべて</label>
-                    </li>
-                    <li class="condition-item" v-for="category in masterData.categories">
-                        <input type="radio" :id="'radCategory' + category.id" :value="category.id" v-model="formValues.category"
-                                                                                                   v-on:change="getGames">
-                        <label :for="'radCategory' + category.id" v-text="category.name"></label>
-                    </li>
-                </ul>
-            </div>
-        </div>
         <div class="col-xs-12 col-sm-12 col-md-8 col-lg-9">
             <div v-if="games.length === 0">
                 <h3>見つかりませんでした。</h3>
