9dd42163a6dac0839ffce419801f97e88b4c9f6d
Subarunari <koyadouhu@gmail.com>
2017-12-20 22:37:57 +0900

    #31 add update action without password to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,4 +35,42 @@ describe User do
     end
   end
 
+  describe "パスワードのみ更新" do
+    let(:user) { build(:user) }
+    subject { user.update_password_only(params) }
+    let(:params) { HashWithIndifferentAccess.new }
+    let(:valid_password) { user.password }
+
+    context "現在のパスワードのみ正しい場合" do
+      it "更新成功" do
+        password_parameter = "testtest"
+        params[:password] = password_parameter
+        params[:password_confirmation] = password_parameter
+        params[:current_password] = valid_password
+        is_expected.to be_truthy
+      end
+
+      it "更新失敗" do
+        params[:current_password] = valid_password
+        is_expected.to be_falsey
+      end
+    end
+
+    context "新しいパスワードのみ正しい場合" do
+      it "更新失敗" do
+        params[:password] = "testtest"
+        params[:password_confirmation] = "testtest"
+        is_expected.to be_falsey
+      end
+    end
+
+    context "新しいパスワードに誤りがある場合" do
+      it "更新失敗" do
+        params[:password] = "validpassword"
+        params[:password_confirmation] = "invalidpassword"
+        params[:current_password] = valid_password
+        is_expected.to be_falsey
+      end
+    end
+  end
 end
