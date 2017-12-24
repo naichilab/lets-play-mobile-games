53dcc1ec2d929b411427b9c6e6c72272fd509cd4
Subarunari <koyadouhu@gmail.com>
2017-12-24 22:59:37 +0900

    #78 add helper method for converting link to a tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,11 @@ module ApplicationHelper
     end
   end
 
+  def text_contain_url(text)
+      URI.extract(text, ['http', 'https']).uniq.each do |url|
+          sub_text = "" << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+          text.gsub!(url, sub_text)
+      end
+      return text
+  end
 end
