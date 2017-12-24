97e69b30da6a9e15c44f186dceb87b6ccab6794b
naichilab <naichilab@live.jp>
2017-12-24 21:21:06 +0900

    Vue全ページ共通の読み込み処理

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,43 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+// ref. https://qiita.com/midnightSuyama/items/efc5441a577f3d3abe74
+// Vue読み込み方
+// 1. 使いたいページのviewで、 data-vue="hogehoge" といったタグをつける
+// 2. packs/options/hogehoge.js を用意する
+//    * dataやmethods等を書いておく
+// たぶんこんなかんじ
+
 console.log('Hello World from Webpacker')
+
+
+import Vue from 'vue/dist/vue.esm'
+import VueDataScooper from "vue-data-scooper"
+
+Vue.use(VueDataScooper)
+
+var vms = []
+
+var options = {}
+var requireContext = require.context('./options', false, /\.js$/)
+requireContext.keys().forEach(key => {
+    let name = key.split('/').pop().split('.').shift()
+    options[name] = requireContext(key).default
+})
+
+document.addEventListener('turbolinks:load', () => {
+    let templates = document.querySelectorAll('[data-vue]')
+    for (let el of templates) {
+        let vm = new Vue(
+            Object.assign(options[el.dataset.vue], {el})
+        )
+        vms.push(vm)
+    }
+})
+
+document.addEventListener('turbolinks:visit', () => {
+    for (let vm of vms) {
+        vm.$destroy()
+    }
+    vms = []
+})
