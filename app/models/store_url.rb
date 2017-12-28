2121ae92af7b8cb6c917ab9cc75309363ca3404c
naichilab <naichilab@live.jp>
2017-12-28 11:40:36 +0900

    #7 store_urlのURLバリデーション追加

--- app/models/store_url.rb
+++ app/models/store_url.rb
@@ -2,6 +2,6 @@ class StoreUrl < ApplicationRecord
   belongs_to :game
   belongs_to :platform
 
-  validates :url, presence: true, length: {maximum: 256}
+  validates :url, presence: true, length: {maximum: 256}, format: /\A#{URI::regexp(%w(http https))}\z/
   validates :memo, length: {maximum: 256}
 end
